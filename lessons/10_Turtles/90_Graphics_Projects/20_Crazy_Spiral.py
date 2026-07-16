2093830bf32f90f6b40f8f7f3de0baa0144ba083
Idktonamedis7 <marioalberto.ramos@alumnos.cumbrestijuana.com>
2026-07-16 02:09:49 +0000

    yes

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -5,25 +5,37 @@ Make your own crazy spiral with a pattern like
 in 14_FLaming_Ninja_Star.py, but use what you've learned about loops
 """
 
-... # Copy code to make a turtle and set up the window
+import random
+import turtle
 
-t = ... # Create a turtle named t
+turtle.setup(600,600,0,0)               # Set the size of the window
+window = turtle.Screen()
 
+t = turtle.Turtle() 
+
+t.shape("turtle") 
+t.speed(0)
+t.goto(0,250)
 # 1) Complete make_a_shape() to make the turtle move in some pattern. 
 # For instance, you can make it go left 30 degrees, then forward 50 pixels, 
 # then right 60 degrees, then forward 100 pixels. Make any shape you like.
 
 def make_a_shape(t):
     """Make a shape with turtle t. Make it go left or right or forward"""    
-    ...
+    t.left(60)
+    t.forward(100)
+    t.right(120)
+    t.forward(200)
 
 # 2) Call make_a_shape() in a loop to make the turtle draw a spiral.
 # For instance, you can call make_a_shape() 100 times to make a spiral with 100 shapes.
 # The second ... in the for loop should be the number of shapes you want to make, 
 # for example 100, or it could use islice(), cycle(), or a list of numbers.
 
-num_shapes = ...
+num_shapes = 100
 
-for i in range(...):
+for i in range(num_shapes):
     make_a_shape(t)
     t.right(360/num_shapes)
+
+ 
